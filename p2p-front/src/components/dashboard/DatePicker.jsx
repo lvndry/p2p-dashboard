dddaa40727b38d7f7344167a9062c068deebd4c0
lvndry <monga.4landry@gmail.com>
2019-08-16 20:38:22 +0200

    datepicker handleChange

--- p2p-front/src/components/dashboard/DatePicker.jsx
+++ p2p-front/src/components/dashboard/DatePicker.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDatePicker from 'react-datepicker';
+import moment from 'moment';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -10,6 +11,16 @@ export default class DatePicker extends React.Component {
             startDate: new Date(2019, 7, 1),
             endDate: new Date(2019, 7, 17),
         };
+        this.handleStartChange = this.handleStartChange.bind(this);
+        this.handleEndChange = this.handleEndChange.bind(this);
+    }
+
+    handleStartChange(date) {
+        this.setState({ startDate: date });
+    }
+
+    handleEndChange(date) {
+        this.setState({ endDate: date });
     }
 
     render() {
@@ -17,12 +28,17 @@ export default class DatePicker extends React.Component {
             <div>
                 <ReactDatePicker
                     selected={this.state.startDate}
+                    dateFormat='dd/MM/yyyy'
+                    onChange={this.handleStartChange}
                 />
                 <div>
                     Zoom
                 </div>
                 <ReactDatePicker
                     selected={this.state.endDate}
+                    dateFormat='dd/MM/yyyy'
+                    onChange={this.handleEndChange}
+                    minDate={this.state.startDate}
                 />
             </div>
         );
