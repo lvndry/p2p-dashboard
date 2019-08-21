a2188d0a41cb71cba760a85613e0915743ecc997
lvndry <monga.4landry@gmail.com>
2019-08-21 09:31:30 +0200

    .env

--- p2p-front/src/lib/http.js
+++ p2p-front/src/lib/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export async function request(method, path, data = {}) {
     const url = `${BASE_URL}${path}`;
